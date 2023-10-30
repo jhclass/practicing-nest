11ffe16d1b62b240d9fe5a2086049adc8f38acf9
Jinhyoung Lee <jhcode0@naver.com>
2023-10-30 19:07:10 +0900

    Use Controller

---
+++ src/movie/movie.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Delete,
+  Patch,
+} from '@nestjs/common';
+
+@Controller('movie')
+export class MovieController {
+  @Get()
+  getAll() {
+    return 'This will return all movies';
+  }
+  @Get('/:id')
+  getOne(@Param('id', ParseIntPipe) id: number) {
+    return `this will return one movie id : ${id}`;
+  }
+  @Post()
+  create() {
+    return 'This will create a movie';
+  }
+  @Delete('/:id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return `This will delete a movie with the id:${id}`;
+  }
+  @Patch('/:id')
+  patch(@Param('id', ParseIntPipe) id: number) {
+    return `this will patch a movie with the id:${id}`;
+  }
+}
