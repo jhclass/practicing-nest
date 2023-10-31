aefed6e2e46c3b2462f71d7dd3d2c68fac7efe43
Jinhyoung Lee <jhcode0@naver.com>
2023-10-31 19:11:28 +0900

    error

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -22,14 +22,23 @@ export class MovieController {
   @Post()
   create(@Body() movieData) {
     console.log(movieData);
-    return 'This will create a movie';
+    //return 'This will create a movie';
+    return movieData;
   }
   @Delete('/:id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return `This will delete a movie with the id:${id}`;
   }
   @Patch('/:id')
-  patch(@Param('id', ParseIntPipe) id: number) {
-    return `this will patch a movie with the id:${id}`;
+  patch(@Param('id') id: string, @Body() updateData) {
+    return {
+      updateMovie: id,
+      ...updateData,
+    };
+  }
+
+  @Get('/search')
+  search() {
+    return `We are searching for a movie with a title`;
   }
 }
