739a5580d7babcbbdc093a38b66175df4a751347
jhclass <jhcode0@naver.com>
2023-12-01 17:03:26 +0900

    Unit test 4

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // dto 에 설정한 유효성 검사 실행 
+      whitelist: true, // dto 에 설정한 유효성 검사 실행
       forbidNonWhitelisted: true, //누군가 이상한걸 보내면 req 자체를 막아버림. ( 다만 , 이 옵션을 사용하려면 whitelist를 사용하여야 함.(true))
       transform: true, //기본적으로 string 을 가져오지만 이 속성을 쓰면 변환해줌
     }),
--- src/movie/movie.service.spec.ts
+++ src/movie/movie.service.spec.ts
@@ -46,4 +46,37 @@ describe("MovieService", () => {
       }
     });
   });
+
+  describe("deleteOne", () => {
+    it("deletes a movie", () => {
+      service.create({
+        title: "testMovie",
+        year: 2024,
+        genres: ["test"],
+      });
+
+      const allMovies = service.getAll();
+      service.deleteOne(1);
+      const afterDelete = service.getAll();
+      expect(afterDelete.length).toEqual(allMovies.length - 1);
+      expect(afterDelete.length).toBeLessThan(allMovies.length);
+    });
+    it("should return a 404", () => {
+      try {
+        service.deleteOne(999);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
 });
+
+/**필기 코멘트 */
+/** 
+ *expect(error).toBeInstanceOf(NotFoundException)와 expect(error.message).toBe("SomeErrorMessage")는 두 가지 다른 테스트 방식입니다.
+expect(error).toBeInstanceOf(NotFoundException)은 error 객체 자체가 NotFoundException 클래스의 인스턴스인지를 확인하는 것입니다. 이 경우, error 객체의 타입만을 검사하며, 에러 메시지나 기타 정보를 고려하지 않습니다. 즉, 에러 객체의 타입만 중요하다고 판단하는 테스트입니다.
+expect(error.message).toBe("SomeErrorMessage")은 에러 객체의 message 속성을 검사하여 특정한 에러 메시지가 예상한 대로 설정되어 있는지 확인합니다. 이 경우, 에러 메시지가 정확하게 일치해야 테스트가 통과합니다.
+따라서 두 테스트 방식은 서로 다른 측면을 검사합니다. 첫 번째 테스트는 예상한 예외 클래스가 발생하는지를 확인하고, 두 번째 테스트는 예외 메시지의 내용이 예상과 일치하는지 확인합니다.
+만약 특정 예외 클래스를 확인하고 동시에 에러 메시지도 검사하려면 다음과 같이 expect 문을 중첩하여 사용할 수 있습니다:
+  
+*/
