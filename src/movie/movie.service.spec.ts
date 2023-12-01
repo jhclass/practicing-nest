b865646629bbec8aaf7307ba21edfdf176047528
jhclass <jhcode0@naver.com>
2023-12-01 16:19:05 +0900

    Unit test 3

--- src/movie/movie.service.spec.ts
+++ src/movie/movie.service.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { MovieService } from "./movie.service";
-
+import { NotFoundException } from "@nestjs/common";
 describe("MovieService", () => {
   let service: MovieService;
 
@@ -20,12 +20,30 @@ describe("MovieService", () => {
     //첫번째 배열을 실행할꺼야.
     it("should return an array", () => {
       // const list = service.create({
-      //   title: "testMovie",
-      //   year: 2024,
-      //   genres: ["test"],
+
       // });
       const result = service.getAll();
       expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe("getOne", () => {
+    it("should return movie", () => {
+      service.create({
+        title: "testMovie",
+        year: 2024,
+        genres: ["test"],
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+    it("should throw 404 error", () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException); //import 필요 (NotFoundException)
+      }
     });
   });
 });
