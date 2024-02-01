eec86411e74f6c8b2c6235d5bd285d712b1669f3
Jinhyoung Lee <jhcode0@naver.com>
2024-02-01 11:23:19 +0900

    코드 정리

--- src/schema/graphql.ts
+++ src/schema/graphql.ts
@@ -1,4 +1,3 @@
-
 /*
  * -------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
@@ -9,62 +8,75 @@
 /* eslint-disable */
 
 export class CommentResponse {
-    ok: boolean;
-    error?: Nullable<string>;
-    message?: Nullable<string>;
+  ok: boolean;
+  error?: Nullable<string>;
+  message?: Nullable<string>;
 }
 
 export abstract class IMutation {
-    abstract createComment(name?: Nullable<string>, content?: Nullable<string>): Nullable<CommentResponse> | Promise<Nullable<CommentResponse>>;
-
-    abstract deleteComment(id: number): Nullable<CommentResponse> | Promise<Nullable<CommentResponse>>;
-
-    abstract getSignedUploadUrl(fileName: string): string | Promise<string>;
-
-    abstract createReportCard(score: number, comment?: Nullable<string>, imageUrl?: Nullable<string>): Nullable<ReportCard> | Promise<Nullable<ReportCard>>;
-
-    abstract createUser(name?: Nullable<string>, password?: Nullable<string>, email?: Nullable<string>): Nullable<CommentResponse> | Promise<Nullable<CommentResponse>>;
-
-    abstract login(name?: Nullable<string>, password?: Nullable<string>): Nullable<LoginResult> | Promise<Nullable<LoginResult>>;
+  abstract createComment(
+    name?: Nullable<string>,
+    content?: Nullable<string>,
+  ): Nullable<CommentResponse> | Promise<Nullable<CommentResponse>>;
+  abstract deleteComment(
+    id: number,
+  ): Nullable<CommentResponse> | Promise<Nullable<CommentResponse>>;
+  abstract getSignedUploadUrl(fileName: string): string | Promise<string>;
+  abstract createReportCard(
+    score: number,
+    comment?: Nullable<string>,
+    imageUrl?: Nullable<string>,
+  ): Nullable<ReportCard> | Promise<Nullable<ReportCard>>;
+  abstract createUser(
+    name?: Nullable<string>,
+    password?: Nullable<string>,
+    email?: Nullable<string>,
+  ): Nullable<CommentResponse> | Promise<Nullable<CommentResponse>>;
+  abstract login(
+    name?: Nullable<string>,
+    password?: Nullable<string>,
+  ): Nullable<LoginResult> | Promise<Nullable<LoginResult>>;
 }
 
 export class Comment {
-    id: number;
-    name: string;
-    content: string;
-    createdAt?: Nullable<string>;
-    updatedAt?: Nullable<string>;
+  id: number;
+  name: string;
+  content: string;
+  createdAt?: Nullable<string>;
+  updatedAt?: Nullable<string>;
 }
 
 export class ReportCard {
-    id: string;
-    score: number;
-    comment?: Nullable<string>;
-    imageUrl?: Nullable<string>;
-    createdAt: DateTime;
-    updatedAt: DateTime;
+  id: string;
+  score: number;
+  comment?: Nullable<string>;
+  imageUrl?: Nullable<string>;
+  createdAt: DateTime;
+  updatedAt: DateTime;
 }
 
 export class TUser {
-    id?: Nullable<number>;
-    tpassword?: Nullable<string>;
-    name?: Nullable<string>;
-    email?: Nullable<string>;
+  id?: Nullable<number>;
+  tpassword?: Nullable<string>;
+  name?: Nullable<string>;
+  email?: Nullable<string>;
 }
 
 export class FindAllResponse {
-    ok: boolean;
-    data?: Nullable<Nullable<TUser>[]>;
+  ok: boolean;
+  data?: Nullable<Nullable<TUser>[]>;
 }
 
 export class LoginResult {
-    ok?: Nullable<boolean>;
-    error?: Nullable<string>;
-    token?: Nullable<string>;
+  ok?: Nullable<boolean>;
+  error?: Nullable<string>;
+  token?: Nullable<string>;
 }
 
 export abstract class IQuery {
-    abstract allUsers(): Nullable<FindAllResponse> | Promise<Nullable<FindAllResponse>>;
+  abstract allUsers():
+    | Nullable<FindAllResponse>
+    | Promise<Nullable<FindAllResponse>>;
 }
 
 export type DateTime = any;
