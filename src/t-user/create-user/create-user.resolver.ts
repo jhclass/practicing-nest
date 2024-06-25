e2f6fbb567ebd587915a77a244f686d9dab843f5
Jinhyoung Lee <jhcode0@naver.com>
2024-06-25 19:52:40 +0900

    create-user.resolver.ts dto 추가

--- src/t-user/create-user/create-user.resolver.ts
+++ src/t-user/create-user/create-user.resolver.ts
@@ -1,5 +1,11 @@
 import { Mutation, Resolver, Args } from "@nestjs/graphql";
 import { CreateUserService } from "@/t-user/create-user/create-user.service";
+export interface CreateUserDto {
+  name: string;
+  password: string;
+  email: string;
+  phoneNum: string;
+}
 @Resolver()
 export class CreateUserResolver {
   constructor(private createUserService: CreateUserService) {}
@@ -10,11 +16,7 @@ export class CreateUserResolver {
     @Args("email") email: string,
     @Args("phoneNum") phoneNum: string,
   ) {
-    return this.createUserService.createUserService(
-      name,
-      password,
-      email,
-      phoneNum,
-    );
+    const createUserDto: CreateUserDto = { name, password, email, phoneNum };
+    return this.createUserService.createUserService(createUserDto);
   }
 }
