47401b3d1bad666c45e1d756328a70be763a278f
Jinhyoung Lee <jhcode0@naver.com>
2024-06-26 12:51:16 +0900

    header 에서 token 이라는 이름의 jwt 토큰을 추출

--- src/jwt.strategy.ts
+++ src/jwt.strategy.ts
@@ -7,7 +7,7 @@ import { ConfigService } from "@nestjs/config";
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private configService: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromHeader("token"), // 'token' 헤더에서 JWT 토큰을 추출
       ignoreExpiration: false,
       secretOrKey: configService.get("SECRET_KEY"), // 환경 변수에서 JWT 비밀키 가져오기
     });
--- src/t-user/t-user.resolver.ts
+++ src/t-user/t-user.resolver.ts
@@ -1,9 +1,11 @@
 import { Query, Resolver } from "@nestjs/graphql";
-
+import { UseGuards } from "@nestjs/common";
 import { TUserService } from "./t-user.service";
+import { gqlAuthGuard } from "@/auth/gql-auth.guard";
 @Resolver()
 export class TUserResolver {
   constructor(private userService: TUserService) {}
+  @UseGuards(gqlAuthGuard)
   @Query()
   async allUsers() {
     return this.userService.findAll();
